98c16ec5237ca1a95c4b49f058d7d3121b4e6ad4
Sergi Hildebrandt <srh@caltech.edu>
2021-11-17 16:18:11 -0800

    Update config_ref.py

--- config_ref.py
+++ config_ref.py
@@ -1,3 +1,3 @@
 # Write here the absolute path of the directory where the Roman_Coronagraph_ETC 
 # is installed 
-INSTALLATION_PATH : 'EXAMPLE:</Users/user_name/.../GitHub/Roman_Coronagraph_ETC/>'
+INSTALLATION_PAT = 'EXAMPLE:</Users/user_name/.../GitHub/Roman_Coronagraph_ETC/>'
